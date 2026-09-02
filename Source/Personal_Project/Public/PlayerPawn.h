c43d05dfdb0d0e306b97b61568e44a0aa783e281
Jomo <71588903+MJomo@users.noreply.github.com>
2026-09-02 10:01:31 +0900

    Add files via upload

--- Source/Personal_Project/Public/PlayerPawn.h
+++ Source/Personal_Project/Public/PlayerPawn.h
@@ -2,6 +2,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Pawn.h"
+#include <GameFramework/FloatingPawnMovement.h>
 #include "PlayerPawn.generated.h"
 
 struct FInputActionValue;
@@ -17,7 +18,7 @@ public:
 
 protected:
 	virtual void BeginPlay() override;
-	UPROPERTY(VisibleAnywhere)
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Anim")
 	class USkeletalMeshComponent* skeleton;
 
 	UPROPERTY(VisibleAnywhere)
@@ -29,12 +30,14 @@ protected:
 	UPROPERTY(VisibleAnywhere)
 	class UCapsuleComponent* Capsule;
 
-
-
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Anim")
+	TSubclassOf<UAnimInstance> AnimInstance;
 
 	virtual void Tick(float DeltaTime) override;
 
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+
 	UFUNCTION()
 	void Move(const FInputActionValue& value);
 
@@ -53,7 +56,44 @@ protected:
 	UFUNCTION()
 	void StopSprint(const FInputActionValue& value);
 
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Movement")
+	bool bIsGrounded = false;
 
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Movement")
+	bool bIsSprinting = false;
 
-	bool bIsGrounded;
+	void CheckGroundState();
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Movement")
+	FVector2D CurrentMoveInput = FVector2D::ZeroVector;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Movement")
+	float WalkSpeed = 400.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Movement")
+	float SprintSpeed = 700.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Movement")
+	float RotationInterpSpeed = 10.0f;
+
+	UPROPERTY(EditAnywhere, Category = "Jump")
+	float JumpVelocity = 700.0f;
+
+	UPROPERTY(EditAnywhere, Category = "Jump")
+	float GravityStrength = 980.0f;
+
+	UPROPERTY(EditAnywhere, Category = "Jump")
+	float JumpCutMultiplier = 0.5f;
+
+	UPROPERTY(EditAnywhere, Category = "Ground")
+	float GroundTraceExtraDistance = 10.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement")
+	float NowMoveSpeed = 0.0f;
+
+	UFloatingPawnMovement* MoveCom;
+
+	float VerticalVelocity = 0.0f;
+
+	void HandleMovement(float DeltaTime);
 };
